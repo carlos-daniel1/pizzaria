3ff2f386a58d50b7a1300cd309637be6861780ab
Vinicius-Firmino <136631514+Vinicius-Firmino@users.noreply.github.com>
2023-11-10 15:19:51 -0300

    Update Main.java
    Adição JOptionPane e pequenos detalhes

--- src/com/grupo/Main.java
+++ src/com/grupo/Main.java
@@ -10,9 +10,7 @@
 package com.grupo;
 
 import java.util.ArrayList;
-
 import javax.swing.JOptionPane;
-
 import com.grupo.entidade.Ingrediente;
 import com.grupo.funcionalidade.Funcionalidade;
 
@@ -39,13 +37,51 @@ public class Main {
 				for (int i=0; i<5; i++) {
 					respostaInt[i] = Integer.parseInt( resposta[i] );
 				}
-				
-				Funcionalidade.criarPizza(mesa, respostaInt);
-			}else if(opcao == 4) {
-				JOptionPane.showMessageDialog(null, Funcionalidade.mostrarIngredientes());
-			}
+
+			} else if (opcao == 2) {
+                		JOptionPane.showMessageDialog(null, "2) Criar um novo pedido", "Criação de pedido", JOptionPane.INFORMATION_MESSAGE);
+
+            		} else if (opcao == 3) {
+                		JOptionPane.showMessageDialog(null, "3) Servir um pedido", "Serviço", JOptionPane.INFORMATION_MESSAGE);
+			} else if (opcao == 4) {
+               			System.out.print("Quantos ingredientes deseja adicionar? \nDigite um número: ");
+
+	               	 	int quantidade = entrada.nextInt();
+	                	entrada.nextLine();
+	
+	                	for (int i = 0; i < quantidade; i++) {
+	
+	                    	System.out.print("Digite o ingrediente: ");
+	                    	String item = entrada.nextLine();
+	
+	                    	if (!ingredientes.isEmpty()) {
+	
+	                        if (!ingredientes.verificarIngrediente(item)) {
+	                            ingredientes.inserir(item);
+	
+	                        } else {
+	                            System.out.println("Ingrediente já cadastrado!");
+	                            break;
+	                        }
+	                    	} else {
+	                        ingredientes.inserir(item);
+	
+	                    }
+	                }
 			
-		}while(opcao!=6);
-	}
+			} else if (opcao == 5) {
+                		JOptionPane.showMessageDialog(null, "5) Estatísticas dos pedidos", "Pedidos", JOptionPane.INFORMATION_MESSAGE);
+
+            		} else if (opcao == 6) {
+                		JOptionPane.showMessageDialog(null, "Agradecemos a preferência, volte sempre!", "Obrigado", JOptionPane.INFORMATION_MESSAGE);
+            		}			
+
+            		else {
+                		JOptionPane.showMessageDialog(null, "Opção inválida. Insira um número.", "Erro", JOptionPane.ERROR_MESSAGE);
+            		}
+
+        }
+        entrada.close();
+    }
 
 }
